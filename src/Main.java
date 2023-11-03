8dea3ecae19692bc787293a90bd211573c81f6f9
elgaricito <edgarjaguirre@hotmail.com>
2023-11-03 17:37:19 -0300

    Changes in the classes and description of the project

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+/*Hardware store
+In this project we have a hardware store that sell different products. We represent this product using classes.
+Now we can se the classes and some example of the kind of product that they are going to content:
+1-ConstructionProduct: nails, screws, cement, etc.
+2-ElectricProduct: cables, electric tape, socket outlet,power drills, hole puncher, etc.
+3-HandTool: hammer, hand saw
+4-GardenProduct:shovel, plant pot
+5-HouseholdItem: kettle, broom, brush
+6-WaterProduct:water filter, water pump
+7-GasProduct: gas valve,etc
+
+Also, we have:
+Employee: the employee of the store.
+Supplier: the one that sell the product.
+Client: the one that buy the product.
+
+We are going to find the full characteristic within each class
+
+ */
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+
         System.out.println("Hello and welcome!");
 
     }
